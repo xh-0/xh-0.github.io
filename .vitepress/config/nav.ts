4009df699040a9856d8a72a62bac4f6231935fdc
nice try <1609909764@qq.com>
2025-01-16 20:56:41 +0800

    md的修改

--- .vitepress/config/nav.ts
+++ .vitepress/config/nav.ts
@@ -14,10 +14,15 @@ export const nav: DefaultTheme.Config['nav'] = [
     text: '技术之家',
     items: [
       { text: '前端', link: '/code/index', activeMatch: '/code/' },
-      { text: 'markdown', link: '/md/index', activeMatch: '/md/' },
     ],
     activeMatch: '/code/', // // 当前页面处于匹配路径下时, 对应导航菜单将突出显示
-
+  },
+  {
+    text: 'markdown',
+    items: [
+      { text: 'markdown', link: '/md/index', activeMatch: '/md/' },
+    ],
+    activeMatch: '/md/', // // 当前页面处于匹配路径下时, 对应导航菜单将突出显示
   },
   { text: '关于我的', link: '/about/about.md', activeMatch: '/about/' },
 ];
